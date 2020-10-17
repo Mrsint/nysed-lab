405ae4cef76493008082396565afd293bff7c288
Shane Sint <ssint@schools.nyc.gov>
2020-10-17 19:26:20 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+import SchoolCard from './components/SchoolCard.js';
+import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
+    console.log(data[0])
   return (
     <div className="App">
       <Navbar/>
       <div className="container">
         <div className="row">
+            {/* <SchoolCard school={data[0]} />
+           <SchoolCard school={data[1]}/>
+           <SchoolCard school={data[2]}/> */}
 
+            {data.map(school =>{
+                return (<SchoolCard school ={school}/>)
+            })}
+         
         </div>
       </div>
     </div>
--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school.ENTITY_NAME}</h5>
         <div className="card-body">
           <h5 className="card-title">
-            <span className="badge">2017-18</span>
-            <span className="badge">2016-17</span>
+            <span className="badge">YEAR: {props.school.YEAR}</span>
+            <span className="badge"></span>
             <span className="badge">Archive</span>
           </h5>
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
+            <li className="list-group-item">Males: {props.school.NUM_MALE}</li>
+            <li className="list-group-item">Females: {props.school.NUM_FEMALE}</li>
             <li className="list-group-item">3-8 Math Assessment Data</li>
           </ul>
           <span className="badge badge-header">School Data</span>
